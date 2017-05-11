d1c219d52a5437f91d701552fb9dd7586b5c074e
Ueli Kunz <kunz@ideadapt.net>
2017-05-11 22:43:29 +0200

    POST /items - use body parser to accept application/json content-type requests. store new item in data.json

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 'use strict'
 import express from 'express'
 import fs from 'fs'
+import bodyParser from 'body-parser'
+
 let app = express()
+app.use(bodyParser.json())
 
 app.get('/items', function (req, res) {
   let items = fs.readFileSync('data.json', 'utf8')
   let payload = JSON.parse(items)
-  res.send(payload)
+  
+  res.json(payload)
+})
+
+app.post('/items', function (req, res) {
+  let payload = req.body
+  let items = fs.readFileSync('data.json', 'utf8')
+  let db = JSON.parse(items)
+
+  db.push(payload)
+
+  fs.writeFileSync('data.json', JSON.stringify(db), {encoding: 'utf8'})
+
+  res.json(payload)
 })
 
 app.listen(3100)
