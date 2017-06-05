0cdaeee7b57425c74b80afc89f825763c485a558
Ueli Kunz <kunz@ideadapt.net>
2017-06-05 18:45:17 +0200

    implement cookie based delete authorization.

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import express from 'express'
 import fs from 'fs'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 
 let app = express()
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.use(cors({
   origin: 'http://localhost:8008',
   credentials: true
@@ -20,6 +22,14 @@ app.get('/items', function (req, res) {
 app.delete('/items/:id/', function (req, res) {
   let items = fs.readFileSync('data.json', 'utf8')
   let db = JSON.parse(items)
+
+  let sessionId = req.cookies['mvc-sess-id']
+  if(!sessionId){
+    res.status(401)
+    res.json({})
+    return
+  }
+
   let idx = db.findIndex((item)=>item.id === req.body.id);
   db.splice(idx, 1)
 
@@ -32,7 +42,11 @@ app.post('/items', function (req, res) {
   let payload = req.body
   let items = fs.readFileSync('data.json', 'utf8')
   let db = JSON.parse(items)
-  
+
+  if(payload.title === 'admin'){
+    res.cookie('mvc-sess-id', '1234', {httpOnly: true})
+  }
+
   db.push(payload)
 
   fs.writeFileSync('data.json', JSON.stringify(db), {encoding: 'utf8'})
